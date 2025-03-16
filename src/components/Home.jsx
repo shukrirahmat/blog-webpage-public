1618c07ad94309419d362ba2e366c7de558b8005
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-03-17 02:18:49 +0800

    feat: recognise user after jwt login

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,50 @@
+import { useState, useEffect } from "react";
+import fetchURL from "../fetchURL";
+
 const Home = () => {
-  return (
-    <div>
-      <h1>THIS IS HOMEPAGE</h1>
-      <p>You are not logged in.</p>
-    </div>
-  );
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [userName, setUserName] = useState(null);
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+
+    //FETCH POSTS
+    if (!token) {
+      setUserLoggedIn(false);
+      setIsLoading(false);
+    } else {
+      fetch(fetchURL + "/user", {
+        mode: "cors",
+        method: "get",
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then((response) => {
+          if (response.ok) return response.json();
+          else throw new Error("Not logged in");
+        })
+        .then((data) => {
+          setUserName(data.username);
+          setUserLoggedIn(true);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setUserLoggedIn(false);
+          setIsLoading(false);
+        });
+    }
+  }, []);
+
+  if (isLoading) {
+    return <div>LOADING PAGE...</div>;
+  } else {
+    return (
+      <div>
+        <h1>THIS IS HOMEPAGE</h1>
+        {userLoggedIn ? <p>HELLO, {userName}</p> : <p>YOU ARE NOT LOGGED IN</p>}
+      </div>
+    );
+  }
 };
 
 export default Home;
