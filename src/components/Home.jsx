0cd96e6d80242bcb2d6e2e14377a0fcdffd971ec
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2025-03-21 00:11:36 +0800

    feat: load posts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,66 @@
+import { useEffect, useState } from "react";
+import fetchURL from "../fetchURL";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return (
-    <div>
-      <h1>THIS IS HOMEPAGE</h1>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(null);
+
+    fetch(fetchURL + "/posts", {
+      mode: "cors",
+      method: "get",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Server Error");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPosts(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading Posts...</p>;
+  } else if (error) {
+    return <p>{error}</p>;
+  } else {
+    return (
+      <div>
+        <h1>POSTS</h1>
+        <ul>
+          {posts.map((post) => {
+            if (post.published) {
+              return (
+                <li key={post.id}>
+                  <div>
+                    <Link to={`/posts/${post.id}`}>
+                      <h3>{post.title}</h3>
+                    </Link>
+                    <h5>
+                      by {post.authorUsername} on {post.datePosted}
+                    </h5>
+                    <p>{post.content}</p>
+                  </div>
+                </li>
+              );
+            }
+          })}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default Home;
